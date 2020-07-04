04c7d19a0c6199cbae8c19cce8b8ad14dec4b251
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-04 19:45:56 +0200

    alle rollen init

--- commands/rollen.js
+++ commands/rollen.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) => {
 
     var botEmbed = new discord.MessageEmbed()
         .setTitle("De rollen zijn:")
-        .setDescription("Owner \n Co-owner \n head discord developer daniël \n Management")
+        .setDescription("Owner \n Co-owner \n head discord developer daniël \n Management \n Team management \n Super admin | HC \n Admin \n Moderator \n Proef moderator \n Head developer \n Skripter \n Developer \n Proef developer \n Teamleider brandweer \n Teamleider politie \n Teamleider ambulance \n Teamleider KMar \n HR | brandweer \n HR | politie \n HR | ambulance \n HR | KMar \n Brandweer \n Politie \n Ambulance \n KMar \n VIP \n DJ \n Burger")
         .setColor("#6dad3e")
         .setThumbnail("https://media.discordapp.net/attachments/719562600282849324/728612375590338630/Schermopname_58.png?width=845&height=475")
         .setImage("https://media.discordapp.net/attachments/719562600282849324/728612375590338630/Schermopname_58.png?width=845&height=475")
