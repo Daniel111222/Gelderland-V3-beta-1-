e4e95d818c3ff0e2efa79afb07da0310bab8b421
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-04 19:31:25 +0200

    Create rollen.js

---
+++ commands/rollen.js
@@ -0,0 +1,48 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    var botEmbed = new discord.MessageEmbed()
+        .setTitle("rollen")
+        .setDescription("Owner \n Co-owner \n head discord developer daniël \n Management")
+        .setColor("#6dad3e")
+        .setThumbnail("https://media.discordapp.net/attachments/719562600282849324/728612375590338630/Schermopname_58.png?width=845&height=475")
+        .setImage("https://media.discordapp.net/attachments/719562600282849324/728612375590338630/Schermopname_58.png?width=845&height=475")
+        .setFooter("Bot maker: @Daniël#2939 - Owner Gelderland: @playfruitje#9860", "https://media.discordapp.net/attachments/719562600282849324/728612375590338630/Schermopname_58.png?width=845&height=475")
+        .setTimestamp();
+        
+    return message.channel.send(botEmbed);
+}
+
+module.exports.help = {
+    name: "rollen"
+}
+
+// burger
+// DJ
+// VIP
+// ambulance
+// brandweer
+// politie
+// KMAR
+// HR | brandweer
+// HR | ambulance
+// HR | politie
+// HR | KMAR
+// Teamleider KMar
+// Teamleider Ambulance
+// Teamleider Politie
+// Teamleider Brandweer
+// proef developer
+// developer
+// skripter
+// head developer
+// proef mod
+// mod
+// admin
+// super admin | HC
+// Team Management
+// Management
+// head discord developer daniël
+// Co-owner
+// owner
