89c8ec0f628a18782905f52491833745e679b6c6
Daniel111222 <66868418+Daniel111222@users.noreply.github.com>
2020-07-06 01:08:35 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -8,6 +8,7 @@ module.exports.run = async (client, message, args) => {
     .addFields(
         { name: "Dit is de bot van Gelderland V3 (beta)", value: "De bot is gemaakt door @Daniël#2939 en de owner van de server is @playfruitje#9860"},
         { name: "Dit is De officiële support server van de bot: Daniël bot", value: "[Klik Hier!](https://discord.gg/2E5fhn3)" },
+        { name: "Dit is de officiële website van de eigenaar van de bot.", value: "[Klik Hier!](https://danielbot.webnode.nl)"}
     )
     .setThumbnail("https://media.discordapp.net/attachments/719562600282849324/728612375590338630/Schermopname_58.png?width=845&height=475")
     .setImage("https://media.discordapp.net/attachments/719562600282849324/728612375590338630/Schermopname_58.png?width=845&height=475")
